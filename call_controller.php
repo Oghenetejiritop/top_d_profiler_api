7cbbc6e978f2eaa81de87b89ea0fa5385328a2d6
Oghenetejiritop <tjsouthernking@gmail.com>
2025-01-05 22:07:02 -0500

    Script that handles the API call base on the request type

---
+++ call_controller.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * call_controller.php
+ *  Script that handles the API call base on the type of request
+ * @author Oghenetejiri Peace Onosajerhe
+ * @version 1.0
+ * @package top_d_profiler_api
+ */
+
+?>
