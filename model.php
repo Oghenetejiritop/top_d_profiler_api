316620d05a5ed93c63dabb964a28c643b4098ba5
Oghenetejiritop <tjsouthernking@gmail.com>
2025-01-05 21:59:33 -0500

    Created the model.php script to interact with the database connection

---
+++ model.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * model.php 
+ * Script that interacts with the database
+ * @author Oghenetejiri Peace Onosajerhe
+ * @version 1.0
+ * @package top_d_profiler_api
+ */
+
+include 'connect_db.php';
+connectDatabase();
+
+
+?>
