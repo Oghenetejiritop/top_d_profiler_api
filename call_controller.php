6a9a955b030456b8176564d5852556a6b025e851
Oghenetejiritop <tjsouthernking@gmail.com>
2025-01-27 14:31:44 -0500

    Added the get request on the controller

--- call_controller.php
+++ call_controller.php
@@ -13,8 +13,25 @@ header('Content-Type: application/json');
  include 'model.php';
  
 
- $method = $_SERVER['REQUEST_METHOD'];
+/**determine  the url request */
+$method = $_SERVER['REQUEST_METHOD'];
 $uri = $_SERVER['REQUEST_URI'];
 
+
+switch($method) {
+    case 'GET':    //validate and extract the user from the get request
+        $user = ["response" => "false"];
+        if(preg_match("/^\/top_d_profiler_api/", $uri)) {
+            $user =access_account(substr($uri, strpos($uri, "s") + 4));
+        }
+        else
+        $user = ["response" => "false $uri"];
+        echo json_encode($user);
+                break;
+        case 'POST':
+            break;
+            default:
+            break;
+}
 
 ?>
